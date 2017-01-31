e62ed5708041276e59684dbde7b2babca00768ea
姚泽源 <yaozeyuan93@gmail.com>
2017-02-01 01:46:57 +0800

    *    修改缩进

--- muilt-page-vue/build/build.js
+++ muilt-page-vue/build/build.js
@@ -10,9 +10,9 @@ var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 
 console.log(
-  '  Tip:\n' +
-  '  Built files are meant to be served over an HTTP server.\n' +
-  '  Opening index.html over file:// won\'t work.\n'
+    '  Tip:\n' +
+    '  Built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
 )
 
 var spinner = ora('building for production...')
@@ -25,13 +25,13 @@ mkdir('-p', assetsPath)
 cp('-R', 'src/assets/', assetsPath);// 注意在这里要将静态资源文件地址换成我们配置的地址
 
 webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n')
+    spinner.stop()
+    if (err) throw err
+    process.stdout.write(stats.toString({
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false
+        }) + '\n')
 })
--- muilt-page-vue/build/dev-server.js
+++ muilt-page-vue/build/dev-server.js
@@ -18,28 +18,29 @@ var app = express()
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
+    publicPath: webpackConfig.output.publicPath,
+    quiet: true
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {}
+    log: () => {
+    }
 })
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
+    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+        hotMiddleware.publish({action: 'reload'})
+        cb()
+    })
 })
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
-  }
-  app.use(proxyMiddleware(context, options))
+    var options = proxyTable[context]
+    if (typeof options === 'string') {
+        options = {target: options}
+    }
+    app.use(proxyMiddleware(context, options))
 })
 
 // handle fallback for HTML5 history API
@@ -59,17 +60,17 @@ app.use(staticPath, express.static('./static'))
 var uri = 'http://localhost:' + port
 
 devMiddleware.waitUntilValid(function () {
-  console.log('> Listening at ' + uri + '\n')
+    console.log('> Listening at ' + uri + '\n')
 })
 
 module.exports = app.listen(port, function (err) {
-  if (err) {
-    console.log(err)
-    return
-  }
+    if (err) {
+        console.log(err)
+        return
+    }
 
-  // when env is testing, don't need open it
-  if (process.env.NODE_ENV !== 'testing') {
-    opn(uri)
-  }
+    // when env is testing, don't need open it
+    if (process.env.NODE_ENV !== 'testing') {
+        opn(uri)
+    }
 })
