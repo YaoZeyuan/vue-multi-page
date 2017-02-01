872bdcf0885dac63778eb664717e11a3b26e3c02
姚泽源 <yaozeyuan93@gmail.com>
2017-02-01 10:11:34 +0800

    *    添加示例demo

---
+++ muilt-page-vue/src/view/demo/router.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yaoze on 2016/9/26.
+ */
+
+// 由于是demo项目，所以暂时不使用路由(没有组件可用)
+// 组件只有import之后才能使用
+
+const routes =
+    [
+        //{
+        //    path: '/',
+        //    component: ,
+        //},
+    ];
+export default routes;
