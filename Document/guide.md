afedeb6ecfdcbafc78fa820b5460bc309d9e92e1
姚泽源 <yaozeyuan93@gmail.com>
2017-02-04 00:09:39 +0800

    *    添加版本控制选项

--- Document/guide.md
+++ Document/guide.md
@@ -1,2 +1,16 @@
 #   muilt-page-vue使用指南
 
+项目说明：
+
+项目基于vue-cli创建，可以作为vue-cli的多页面版使用
+
+使用方法:
+
+1.  下载项目，执行`npm install`安装依赖
+
+2.  复制`./src/view/demo`目录为任意新项目名，在`/config/project`的project属性中添加【项目名】=>【项目路径】的对应关系，即可开始新页面的编辑
+
+3.  执行`npm run dev`进入开发模式，`http://localhost:8080/【项目名】/【tag号】/html/【页面名】/index.html`即为对应的调试地址，档本地文件后，系统会自动重新打包编译并载入新的vue代码，非常方便
+调试地址示例[http://localhost:8080/multi_vue_demo/0.0.2/html/demo/index.html](http://localhost:8080/multi_vue_demo/0.0.2/html/demo/index.html)
+
+4.
--- config/project.js
+++ config/project.js
@@ -4,12 +4,25 @@
 
 // 获取项目版本号，用以生成目录
 let fs = require("fs")
-// hg项目获取版本号
-// let tag_content = fs.readFileSync("./.hgtags", 'utf8') // 获取hgtags的内容
-// let version = tag_content.trim().split(' ').pop() // 直接trim后取最后一行，以空格分隔获取最新版本号
-// git项目获取版本号
-let version = fs.readdirSync('./.git/refs/tags').pop().trim()// 获取版本库最新tag值
+
 let name = 'multi_vue_demo' // 项目名，提取出来以方便dev环境使用，否则会出现undefined = =
+let vcc_type = 'git' // 版本控制工具
+
+let version = ''
+if (vcs_type == 'git') {
+    // git项目获取版本号
+    version = fs.readdirSync('./.git/refs/tags').pop().trim()// 获取版本库最新tag值
+}
+else {
+    if (vcs_type == 'hg') {
+        // hg项目获取版本号
+        let tag_content = fs.readFileSync("./.hgtags", 'utf8') // 获取hgtags的内容
+        version = tag_content.trim().split(' ').pop() // 直接trim后取最后一行，以空格分隔获取最新版本号
+    } else {
+        version = '0.0.1'
+    }
+}
+
 
 module.exports = {
     // 方便当出现多项目、多页面需求时的进一步扩展。不能与其他项目重名
