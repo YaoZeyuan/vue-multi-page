8c070e99c249c079b2a76cc8bc4140554e520c16
姚泽源 <yaozeyuan93@gmail.com>
2017-03-02 14:44:13 +0800

    feat : 初始化微信Editor

---
+++ src/view/weixin_editor/components/list/article_list.vue
@@ -0,0 +1,84 @@
+<template>
+    <!-- vue要求每个组件中都有且只能有一个根元素 -->
+    <div class="base">
+        <div class='article-list'>
+            <template v-for="post in post_list">
+                <preview :post=post></preview>
+            </template>
+        </div>
+        <load-data v-if="!is_completed" :is_loading="is_loading"></load-data>
+    </div>
+</template>
+
+<style scoped>
+    /*  style标签之内会被作为css进行处理，所以需要使用css的注释形式   */
+    /*  加上scoped可以将css的作用效果限定在该组件之内 */
+    .base {
+        text-align: center;
+        font-family: sans-serif;
+        margin-top: 2rem;
+    }
+</style>
+
+<script>
+    import preview from './preview.vue'
+    import loadData from './load_data.vue'
+
+    export default {
+        data: function () {
+            return {
+                post_list: [], // 这里使用了数组作为加载
+                page: 0,
+                total: 0,
+                is_loading: false,// 是否正在加载数据
+            }
+        },
+        mounted: function () {
+            this.getPosts();
+        },
+        methods: {
+            getPosts: function () {
+                if (this.is_completed) {
+                    console.info(`全部数据已加载完毕，不需要再请求数据了`)
+                    return
+                }
+                if (this.is_loading) {
+                    console.info(`数据正在加载中，请稍后再试`)
+                    return
+                }
+                this.is_loading = true;
+                this.$http.jsonp('http://www.yaozeyuan.online/api/get_posts/', {
+                    page: this.page
+                }).then((response) => {
+                    // success callback
+                    console.log(response.body)
+
+                    this.is_loading = false;
+                    if (response.body.status == 'ok') {
+                        // 获取成功
+                        this.post_list = response.body.posts
+                    } else {
+                        console.info(`获取文章列表失败` + '失败原因:返回值错误');
+                    }
+                }, (response) => {
+                    console.info(`获取文章列表失败` + '失败原因:网络异常');
+
+                    this.is_loading = false;
+                });
+            }
+        },
+        computed:{
+            is_completed:function () {
+                // 我显然写过博客。。。
+                return this.total == this.post_list.length && this.total != 0
+            }
+
+
+        },
+        components: {
+            'preview': preview,
+            loadData,// 也可以这样导入
+            // 驼峰式命名的组件会被自动转换成横线分隔的组件名
+        }
+    }
+</script>
---
+++ src/view/weixin_editor/main.vue
@@ -0,0 +1,30 @@
+<!-- 基础目录组件，用于渲染路由页面 -->
+<template>
+    <div>
+        <!-- 路由出口 -->
+        <!-- 路由匹配到的组件将渲染在这里 -->
+        <div>
+            <!--实际页面内容-->
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<style >
+/*  style标签之内会被作为css进行处理，所以需要使用css的注释形式   */
+/*  加上scoped可以将css的作用效果限定在该组件之内 */
+    html{
+        /* 添加到这里，为网页加上背景色 */
+        background-color: rgb(233,242,249);
+    }
+</style>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                msg: 'Use Vue 2.0 Today!'
+            }
+        },
+    }
+</script>
